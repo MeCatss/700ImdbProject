6202ad7f6ad8a35a210be8be021a8132c2c9cda9
Comal <akmalfaiq0@gmail.com>
2026-08-16 16:37:14 +0700

    Created endpoints to call movie datas

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,37 @@
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI()
 
+df = pd.read_csv("../data/IMDb_Top_700_Movies_2026_cleaned.csv")
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def main():
+    return {"Supp Hello World"}
 
+@app.get("/movies")
+def read_movies():
+    return {"movies": df.to_dict(orient="records")}
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: str | None = None):
-#     return {"item_id": item_id, "q": q}
+@app.get("/movies/{rank}")
+def read_movie_by_rank(rank: int):
+    movie = df[df['Rank'] == rank]
+    if not movie.empty:
+        return {"movie": movie.to_dict(orient="records")[0]}
+    else:
+        return {"error": "Movie not found"}
+
+@app.get("/stats/genres")
+def genres_count():
+    genres = df['Genre(s)'].str.split('|').explode().value_counts().to_dict()
+    return {"genres": genres}
+
+@app.get("/stats/decades")
+def decades_avg_rating():
+    avg_ratings = df.groupby('Decade')['IMDb Rating'].mean().to_dict()
+    return {"avg_ratings_by_decade": avg_ratings}
+
+@app.get("/stats/topdirectors")
+def top_directors_by_movies():
+    directors = df['Director'].value_counts().head(10).to_dict()
+    return {"top_directors": directors}
