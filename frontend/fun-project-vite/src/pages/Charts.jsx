2f0f9fef1665e85245ed473221b3c167ec924fa6
Comal <akmalfaiq0@gmail.com>
2026-08-21 18:33:18 +0700

    finished fetching data in charts

--- frontend/fun-project-vite/src/pages/Charts.jsx
+++ frontend/fun-project-vite/src/pages/Charts.jsx
@@ -5,32 +5,32 @@ const Charts = () => {
     const [genres, setgenres] = useState([]);
     useEffect(() => {
         const getgenres = async () => {
-        const res = await axios('http://127.0.0.1:8000/movies');
+        const res = await axios('http://127.0.0.1:8000/stats/genres');
         console.log(res.data);
         setgenres(res.data);
         };
         getgenres();
-    }, []);
+    }, []); // To get how many genres valuecount in the database
 
-    // const [moviedata, setmoviedata] = useState([]);
-    // useEffect(() => {
-    //     const getMovie = async () => {
-    //     const res = await axios('http://127.0.0.1:8000/movies');
-    //     console.log(res.data);
-    //     setmoviedata(res.data);
-    //     };
-    //     getMovie();
-    // }, []);
+    const [decade, setdecade] = useState([]);
+    useEffect(() => {
+        const getDecade = async () => {
+        const res = await axios('http://127.0.0.1:8000/stats/decades');
+        console.log(res.data);
+        setdecade(res.data);
+        };
+        getDecade();
+    }, []); // To get average rating per decade.
 
-    // const [moviedata, setmoviedata] = useState([]);
-    // useEffect(() => {
-    //     const getMovie = async () => {
-    //     const res = await axios('http://127.0.0.1:8000/movies');
-    //     console.log(res.data);
-    //     setmoviedata(res.data);
-    //     };
-    //     getMovie();
-    // }, []);
+    const [directors, setdirectors] = useState([]);
+    useEffect(() => {
+        const getDirectors = async () => {
+        const res = await axios('http://127.0.0.1:8000/stats/topdirectors');
+        console.log(res.data);
+        setdirectors(res.data);
+        };
+        getDirectors();
+    }, []); // To get how many movies top 10 director has directed the most.
 
     return (
             <h1>Charts</h1>
