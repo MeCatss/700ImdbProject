9c8f35c5c2ba1946ba62018d723c2ee7aac629f4
Comal <akmalfaiq0@gmail.com>
2026-09-01 12:32:37 +0700

    Adding might also like feature when navigate to moviedetail

--- frontend/fun-project-vite/src/pages/MovieDetail.jsx
+++ frontend/fun-project-vite/src/pages/MovieDetail.jsx
@@ -1,20 +1,31 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
 import {useParams} from 'react-router-dom'
+import { useNavigate } from 'react-router'
 
 const MovieDetail = () => {
-
-    const{rank}=useParams()
+    let navigate = useNavigate();
+    const {rank}=useParams()
     const [moviedetail, setmoviedetail] = useState([]);
     useEffect(() => {
         const getMovieDetail = async () => {
-        const res = await axios(`http://127.0.0.1:8000/movies/1`); 
-        //////////// 1 is not supposed to be hardcoded
+        const res = await axios(`http://127.0.0.1:8000/movies/${rank}`); 
         console.log(res.data.movie);
         setmoviedetail(res.data.movie);
         };
         getMovieDetail();
-    }, []);
+    }, [rank]);
+
+    const [movierec, setmovierec] = useState([]);
+    useEffect(() => {
+        if(!moviedetail.Title) return
+        const getmovierec = async () => {
+        const res = await axios(`http://127.0.0.1:8000/recommendations/${encodeURIComponent(moviedetail.Title)}`)
+        console.log(res.data.movie,123323)
+        setmovierec(res.data.movie)    
+        };
+        getmovierec()
+    }, [moviedetail])
 
     return (
         <div>
@@ -22,8 +33,7 @@ const MovieDetail = () => {
                 style={{border: '10px solid black',
                         margin: '20px',
                         padding: '10px'
-                        }}
-                >
+                        }}>
                     <h1>Movie Detail</h1>
                     <h3>{moviedetail.Title}</h3>
                     <p>Rank: {moviedetail.Rank}</p>
@@ -35,6 +45,18 @@ const MovieDetail = () => {
                     <p>Year: {moviedetail["Year"]}</p>
                     <p>Rating: {moviedetail["IMDb Rating"]}   Votes: {moviedetail.Votes}</p>
                 </div>
+
+                {movierec.map((movie) => (
+                <div key={movie.Rank} 
+                style={{border: '10px solid black',
+                        margin: '20px',
+                        padding: '10px',
+                        cursor:"pointer"}}
+                onClick={() => navigate(`/movies/${movie.Rank}`)}
+                >
+                    <h3>{movie.Title}</h3>
+                </div>
+            ))}
             
         </div>
     )
